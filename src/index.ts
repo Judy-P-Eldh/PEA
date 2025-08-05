b3743bdf7d782b612a8726bbe03e287262042206
ebobic <ebobic95@gmail.com>
2025-08-05 16:49:22 +0200

    Lagt till funktionalitet för att visa alla studentnamn i listan

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import { students } from "./student.js";
 const content = document.querySelector("#content") as HTMLElement;
-console.log(content);
 
 function ShowStudents(): void {
-  console.log(students);
+  let info = document.createElement("p");
 
   if (students.length < 1) {
-    let info = document.createElement("p");
-    info.innerText = "Listan är tom";
-    console.log("vi kommer hit");
+    info.innerText = "Listan är tom";   // Om det är färre än 1 student, visa då texten ''listan är tom''
     content.appendChild(info);
   }
+// Loopar igenom listan och skapat ett p-element vid varje loop & sedan infogar den innehållet från listan in i p-tagen
+students.forEach(student => { 
+    let namn = document.createElement("p");
+    namn.innerText = student.name;
+    content.appendChild(namn);
+});
 }
 
 ShowStudents();
